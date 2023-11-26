091c0532e857d92a73c3e1589137268b347ce953
Franck Binard <franckbinard@gmail.com>
2023-11-26 18:43:08 +0000

    added handlebars support

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,16 @@
 import express, { Request, Response } from 'express';
+import {engine} from 'express-handlebars';
 
 const app = express();
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+
 const port = process.env.PORT || 3000;
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello, TypeScript Express!');
+// Routes
+app.get('/', (req, res) => {
+    res.render('home');
 });
-
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
