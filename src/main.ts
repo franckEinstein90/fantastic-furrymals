f55da3995eddb202ed8a632cd2d05983843a96d7
Franck Binard <franckbinard@gmail.com>
2023-12-22 13:49:42 -0500

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,17 @@
+/******************************************************************************
+ * The entry point for the llm-deliotte-chat-bot application. 
+ * The appliation is a chat bot that is hosted on Azure and is accessible
+ * through http or websocket connections. 
+ * December 2023
+/******************************************************************************/
+// <----------------------------- 1. Load Environment Variables ---------------->
 import { appFactory } from './server/appFactory';
-import winston from 'winston'
+import * as path from 'path';
+import {config} from 'dotenv';
+import winston from 'winston';
+const ENV_FILE_NAME = process.env.ENV_FILE || '.env';
+const ENV_FILE_PATH = path.join(__dirname, '..', ENV_FILE_NAME);
+config({ path: ENV_FILE_PATH });
 
 // Create a logger instance
 const logger = winston.createLogger({
